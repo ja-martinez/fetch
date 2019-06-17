6374b4feb2422896360eb69cd2e46188ac68552b
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-06-17 16:53:55 -0700

    added public assests and veiw for the home page

--- controllers/template.js
+++ controllers/template.js
@@ -2,7 +2,7 @@ const knex = require("../db/knex.js");
 
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
-  index: function(req, res) {
-    res.send("Hello");
+  index: (req, res) => {
+    res.render("index")
   },
 }
