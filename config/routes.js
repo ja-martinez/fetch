d5ffd18e9fcd842e1486dfa33930f21ff385337a
Seth Ortiz <sethl.ortiz@gmail.com>
2019-06-17 15:29:32 -0700

    updated routes.js and created sessions.js

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,26 @@
 //Update the name of the controller below and rename the file.
-const template = require("../controllers/template.js")
+const flights = require("../controllers/flights.js")
+const flights = require("../controllers/watchList.js")
 module.exports = function(app){
 
-  app.get('/', template.index);
+  app.get('/', flights.goHome);
+  app.get('/user/login', flights.getLogin);
+  app.get('/user/:id', flights.getFlight);
+  app.post('/user/:id', appointments.create);
+  app.post('/register', flights.register);
+  app.post('/login', flights.login);
+  app.use(authMiddleware);
+  app.get('/myFlights',watchList.myFlights)
 
 }
+
+function authMiddleware(req, res, next){
+  if(!req.session.user_id){
+    res.redirect("/user_login");
+    
+  }else{
+    next();
+  }
+}
+
+app.use(authMiddleware);
