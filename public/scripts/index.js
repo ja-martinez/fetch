624c6b61f19b69a30c6a185cd88a2e151d6c0e4a
Seth Ortiz <sethl.ortiz@gmail.com>
2019-06-18 11:01:40 -0700

    Changed log in to registe and created a scripts

---
+++ public/scripts/index.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  document.getElementById("password1").onchange = validatePassword;
+  document.getElementById("password2").onchange = validatePassword;
+};
+function validatePassword() {
+  var pass2 = document.getElementById("password2").value;
+  var pass1 = document.getElementById("password1").value;
+  if (pass1 != pass2)
+    document
+      .getElementById("password2")
+      .setCustomValidity("Passwords Don't Match");
+  else document.getElementById("password2").setCustomValidity("");
+}
