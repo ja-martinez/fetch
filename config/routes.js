163efa2f3917b95c8fe063a5e51a80acb67a8a02
Alejandro Martinez <j.alejandro.mrtnz@gmail.com>
2019-06-18 11:11:24 -0700

    modify routes and create flight search controller function

--- config/routes.js
+++ config/routes.js
@@ -3,18 +3,18 @@ const flights = require("../controllers/flights.js")
 const watchList = require("../controllers/watchList.js")
 module.exports = function(app){
 
-  app.get('/', flights.goHome);
-  // app.get('/fetch/flights', flights.getFlights);
-  // app.get('/fetch/flights/:id', flights.getOne);
-  // app.get('/login', flights.getLogin);
-  // app.get('/fetch/cart', flights.getCart);
-  // app.get('/fetch/checkout', flights.getCheckout)
-  // app.post('/login', flights.login);
-  // app.post('/register', flights.register);
-  // app.use(authMiddleware);
-  // app.post('/fetch/checkout', flights.purchase)
-  // app.get('/myFlights',watchList.myFlights)
-  // app.post('/fetch/flights/:id', watchList.Add);
+  app.get('/', flights.home);
+  app.get('/flights/search"', flights.getFlights);
+  app.get('/flights/:carrier/:flightNumber', flights.getOne);
+  app.get('/login', flights.getLogin);
+  app.get('/cart', flights.getCart);
+  app.get('/checkout', flights.getCheckout)
+  app.post('/login', flights.login);
+  app.post('/register', flights.register);
+  app.use(authMiddleware);
+  app.post('/checkout', flights.purchase)
+  app.get('/myFlights',watchList.myFlights)
+  app.post('/flights/:origin/:destination/:date', watchList.Add);
 }
 
 function authMiddleware(req, res, next){
--- controllers/flights.js
+++ controllers/flights.js
@@ -1,12 +1,106 @@
 const knex = require("../db/knex.js");
+let flights = [];
 
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
-  goHome: (req, res) => {
+  home: (req, res) => {
     res.render("index")
   },
-  
+
   getLogin: (req, res) => {
-    res.render("login")
+    const cabinClass = req.body.cabinClass;
+    const adults = req.body.adults;
+    const children = req.body.children;
+    const infants = req.body.infants;
+    const country = req.body.country;
+    const currency = req.body.currency;
+    const locale = req.body.locale;
+    const originPlace = req.body.origin;
+    const destinationPlace = req.body.destination;
+    const outboundDate = req.body.outboundDate;
+    const inboundDate = req.body.inboundDate;
+    
+
+    unirest.post("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0")
+      .header("X-RapidAPI-Host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
+      .header("X-RapidAPI-Key", "4f35d21e85mshd99b830c3d8fc00p1bb8d5jsn462cba9bbdea")
+      .header("Content-Type", "application/x-www-form-urlencoded")
+      .send(`cabinClass=${cabinClass}`)
+      .send(`children=${children}`)
+      .send(`infants=${infants}`)
+      .send(`country=${country}`)
+      .send(`currency=${currency}`)
+      .send(`locale=${locale}`)
+      .send(`originPlace=${originPlace}`)
+      .send(`destinationPlace=${destinationPlace}`)
+      .send(`outboundDate=${outboundDate}`)
+      .send(`inboundDate=${inboundDate}`)
+      .send(`adults=${adults}`)
+      .end(function (result) {
+        const sessionKey = result.headers.location.slice(-36);
+
+        unirest.get(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/${sessionKey}?sortType=price&sortOrder=asc&pageIndex=0&pageSize=10`)
+          .header("X-RapidAPI-Host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
+          .header("X-RapidAPI-Key", "4f35d21e85mshd99b830c3d8fc00p1bb8d5jsn462cba9bbdea")
+          .end(function (result) {
+            const itineraries = result.body.Itineraries;
+            const legs = result.body.Legs;
+            const carriers = result.body.Carriers;
+            const agents = result.body.Agents;
+            const places = result.body.Places;
+            let originId = '';
+            let destinationId = '';
+            let carrierId = '';
+
+            for (let i = 0; i < itineraries.length; i++) {
+              let flight = {};
+              const agentId = itineraries[i].PricingOptions[0].Agents[0];
+              flight.price = itineraries[i].PricingOptions[0].Price;
+              const legId = itineraries[i].OutboundLegId;
+
+
+              for (let j = 0; j < legs.length; j++) {
+                if (legs[j].Id === legId) {
+                  originId = legs[j].OriginStation;
+                  destinationId = legs[j].DestinationStation;
+                  flight.departureTime = legs[j].Departure;
+                  flight.arrivalTime = legs[j].Arrival;
+                  flight.duration = legs[j].Duration;
+                  flight.numStops = legs[j].Stops.length;
+                  carrierId = legs[j].FlightNumbers[0].CarrierId;
+                  flight.flightNumber = legs[j].FlightNumbers[0].FlightNumber;
+                  break;
+                }
+              }
+
+              for (let j = 0; j < carriers.length; j++) {
+                if (carriers[j].Id === carrierId) {
+                  flight.carrierName = carriers[j].Name;
+                  flight.carrierImgUrl = carriers[j].ImageUrl
+                  break;
+                }
+              }
+
+              for (let j = 0; j < agents.length; j++) {
+                if (agents[j].Id === agentId) {
+                  flight.agentName = agents[j].Name;
+                  flight.agentImgUrl = agents[j].ImageUrl;
+                  break;
+                }
+              }
+
+              for (let j = 0; j < places.length; j++) {
+                if (places[j].Id === originId) {
+                  flight.originName = places[j].Name;
+                } else if (places[j].Id === destinationId) {
+                  flight.destinationName = places[j].Name;
+                }
+
+              }
+              flights.push(flight);
+            }
+            res.render("login", {flights: flights});
+          });
+      })
   }
 }
