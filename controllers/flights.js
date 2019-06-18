6a66f0642ec2dd6f15f811c5863c940897553e60
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-06-17 17:50:14 -0700

    updated watchList controller

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,7 +2,7 @@ const knex = require("../db/knex.js");
 
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
-  index: (req, res) => {
+  goHome: (req, res) => {
     res.render("index")
   },
 }
