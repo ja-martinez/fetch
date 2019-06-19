18a6fd08fbd841ea849e68b9b579a40787e90e64
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-06-19 13:53:37 -0700

    added team and mission sections to home page

--- config/routes.js
+++ config/routes.js
@@ -4,17 +4,17 @@ const watchList = require("../controllers/watchList.js")
 module.exports = function(app){
 
   app.get('/', flights.home);
-  app.get('/flights/search"', flights.getFlights);
-  app.get('/flights/:carrier/:flightNumber', flights.getOne);
-  app.get('/login', flights.getLogin);
-  app.get('/cart', flights.getCart);
-  app.get('/checkout', flights.getCheckout)
-  app.post('/login', flights.login);
-  app.post('/register', flights.register);
-  app.use(authMiddleware);
-  app.post('/checkout', flights.purchase)
-  app.get('/myFlights',watchList.myFlights)
-  app.post('/flights/:origin/:destination/:date', watchList.Add);
+  // app.get('/flights/search"', flights.getFlights);
+  // app.get('/flights/:carrier/:flightNumber', flights.getOne);
+  // app.get('/login', flights.getLogin);
+  // app.get('/cart', flights.getCart);
+  // app.get('/checkout', flights.getCheckout)
+  // app.post('/login', flights.login);
+  // app.post('/register', flights.register);
+  // app.use(authMiddleware);
+  // app.post('/checkout', flights.purchase)
+  // app.get('/myFlights',watchList.myFlights)
+  // app.post('/flights/:origin/:destination/:date', watchList.Add);
 }
 
 function authMiddleware(req, res, next){
