612c6752e9d8ae01dcb97af1841df42a0222bffb
Alejandro Martinez <j.alejandro.mrtnz@gmail.com>
2019-06-19 16:15:14 -0700

    add radio input to home form

--- config/routes.js
+++ config/routes.js
@@ -4,10 +4,9 @@ const watchList = require("../controllers/watchList.js")
 module.exports = function(app){
 
   app.get('/', flights.home);
-<<<<<<< HEAD
   // app.get('/flights/search"', flights.getFlights);
-  // app.get('/flights/:carrier/:flightNumber', flights.getOne);
-  // app.get('/login', flights.getLogin);
+  // app.get('/flights/:flightsIndex', flights.getOne);
+  // app.get('/register', flights.getLogin);
   // app.get('/cart', flights.getCart);
   // app.get('/checkout', flights.getCheckout)
   // app.post('/login', flights.login);
@@ -16,19 +15,6 @@ module.exports = function(app){
   // app.post('/checkout', flights.purchase)
   // app.get('/myFlights',watchList.myFlights)
   // app.post('/flights/:origin/:destination/:date', watchList.Add);
-=======
-  app.get('/flights/search"', flights.getFlights);
-  app.get('/flights/:flightsIndex', flights.getOne);
-  app.get('/register', flights.getLogin);
-  app.get('/cart', flights.getCart);
-  app.get('/checkout', flights.getCheckout)
-  app.post('/login', flights.login);
-  app.post('/register', flights.register);
-  app.use(authMiddleware);
-  app.post('/checkout', flights.purchase)
-  app.get('/myFlights',watchList.myFlights)
-  app.post('/flights/:origin/:destination/:date', watchList.Add);
->>>>>>> b078d2c7b259dc34c336c32480e9f887445e51f1
 }
 
 function authMiddleware(req, res, next){
